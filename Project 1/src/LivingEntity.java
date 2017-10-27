4f0ce75a52a92ad1f31c33c9d3019c0d23eac993
hsmarch <33072551+hsmarch@users.noreply.github.com>
2017-10-27 08:43:20 -0400

    Added method stubs for R3

--- Project 1/src/LivingEntity.java
+++ Project 1/src/LivingEntity.java
@@ -1,3 +1,5 @@
+import jdk.jshell.spi.ExecutionControl;
+
 import java.util.ArrayList;
 
 public class LivingEntity extends Entity {
@@ -19,5 +21,14 @@ public class LivingEntity extends Entity {
 
     public void setMoments(ArrayList<Moment> friends) {
         this.moments = moments;
+    }
+
+    public ArrayList findMaximumCliqueOfFriends() throws ExecutionControl.NotImplementedException {
+        //returns a list containing the maximum clique of friends with whom the target person / pet could go out with, such that each of his/her friends is also friends with everyone else in the set.
+        throw new ExecutionControl.NotImplementedException("Method Not Implemented.");
+    }
+    public static boolean isClique(ArrayList set) throws ExecutionControl.NotImplementedException {
+        //returns true if and only if all the people / pets in the specified set are ALL friends with each other.
+        throw new ExecutionControl.NotImplementedException("Method not Implemented. ");
     }
 }
