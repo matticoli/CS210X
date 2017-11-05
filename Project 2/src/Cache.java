4bb104df4e23cffc76c4835ccf1baae20e70fe91
henryposkanzer <hposkanzer@gmail.com>
2017-11-05 17:17:27 -0500

    added a method that caches should have

--- Project 2/src/Cache.java
+++ Project 2/src/Cache.java
@@ -9,4 +9,5 @@ interface Cache<T, U> extends DataProvider<T, U> {
      * @return the number of cache misses since the object's instantiation.
      */
     int getNumMisses();
+    U get(T key);
 }
