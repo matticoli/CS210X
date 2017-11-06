4eef321d48f83cda2e87b09bdc0dc11c719487ae
Harrison March <hsmarch@wpi.edu>
2017-11-05 19:01:24 -0500

    Attempted O(1) .get(). Probably O(n)

--- Project 2/src/LRUCache.java
+++ Project 2/src/LRUCache.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * An implementation of <tt>Cache</tt> that uses a least-recently-used (LRU)
  * eviction policy.
@@ -7,11 +11,16 @@ public class LRUCache<T, U> implements Cache<T, U> {
      * @param provider the data provider to consult for a cache miss
      * @param capacity the exact number of (key,value) pairs to store in the cache
      */
-    private DataProvider provider;
+    private DataProvider<T,U> provider;
     private int capacity;
     private int numMisses = 0;
+    private HashMap<T,U> dataset;
+    private LinkedList<T> queue;
+
 
     public LRUCache(DataProvider<T, U> provider, int capacity) {
+        dataset = new HashMap<T,U>(capacity);
+        queue = new LinkedList<T>();
         this.provider = provider;
         this.capacity = capacity;
     }
@@ -23,7 +32,22 @@ public class LRUCache<T, U> implements Cache<T, U> {
      * @return the value associated with the key
      */
     public U get(T key) {
-        return null;  // TODO -- implement!
+        U x = null;
+        if (!dataset.containsKey(key)){
+            if (dataset.size() == capacity) {
+                    dataset.remove(queue.remove());
+            }
+          x = provider.get(key);
+            dataset.put(key,x);
+            queue.add(key);
+            numMisses++;
+        }
+        else{
+            x = dataset.get(key);
+            queue.remove(queue.indexOf(key));
+            queue.add(key);
+        }
+        return x;  // TODO -- implement!
     }
 
     /**
