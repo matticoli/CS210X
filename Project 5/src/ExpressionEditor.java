a76039cc3a5747329776c7db219afd569ae1282a
Harrison March <hsmarch@wpi.edu>
2017-12-14 04:07:35 -0500

    I give up. TODO list is in handle function of expressioneditor. Still no luck with current issue: JavaFX does bounding boxes weirdly. Check console for clarification.

--- Project 5/src/ExpressionEditor.java
+++ Project 5/src/ExpressionEditor.java
@@ -22,6 +22,67 @@ public class ExpressionEditor extends Application {
     static Pane expressionPane = new Pane();
 
     /**
+     * Size of the GUI
+     */
+    private static final int WINDOW_WIDTH = 1080, WINDOW_HEIGHT = 250;
+    /**
+     * Initial expression shown in the textbox
+     */
+    private static final String EXAMPLE_EXPRESSION = "2*x+3*y+4*z+(7+6*z)";
+    /**
+     * Parser used for parsing expressions.
+     */
+    private final ExpressionParser expressionParser = new SimpleExpressionParser();
+    //private static final String EXAMPLE_EXPRESSION = "a+b+c";
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Expression Editor");
+        // Add the textbox and Parser button
+        final Pane queryPane = new HBox();
+        final TextField textField = new TextField(EXAMPLE_EXPRESSION);
+        final Button button = new Button("Parse");
+        queryPane.getChildren().add(textField);
+        // Add the callback to handle when the Parse button is pressed
+        button.setOnMouseClicked(e -> {
+            // Try to parse the expression
+            try {
+                // Success! Add the expression's Node to the expressionPane
+                final Expression expression = expressionParser.parse(textField.getText(), true);
+                expressionPane.getChildren().clear();
+                Node node = expression.getNode();
+                expressionPane.setStyle("-fx-font:36 \"Arial\";");
+                node.setLayoutX(WINDOW_WIDTH / 4);
+                node.setLayoutY(WINDOW_HEIGHT / 4);
+                node.applyCss();
+                node.toFront();
+                expressionPane.getChildren().add(node);
+                // If the parsed expression is a CompoundExpression, then register some callbacks
+                if (expression instanceof CompoundExpression) {
+                    ((Pane) expression.getNode()).setBorder(Expression.NO_BORDER);
+                    final MouseEventHandler eventHandler = new MouseEventHandler(expressionPane, (CompoundExpression) expression);
+                    expressionPane.setOnMousePressed(eventHandler);
+                    expressionPane.setOnMouseDragged(eventHandler);
+                    expressionPane.setOnMouseReleased(eventHandler);
+                }
+            } catch (ExpressionParseException epe) {
+                // If we can't parse the expression, then mark it in red
+                textField.setStyle("-fx-text-fill: red");
+            }
+        });
+        queryPane.getChildren().add(button);
+        // Reset the color to black whenever the user presses a key
+        textField.setOnKeyPressed(e -> textField.setStyle("-fx-text-fill: black"));
+        final BorderPane root = new BorderPane();
+        root.setTop(queryPane);
+        root.setCenter(expressionPane);
+        expressionPane.applyCss();
+        expressionPane.layout();
+        primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
+        primaryStage.show();
+    }
+
+    /**
      * Mouse event handler for the entire pane that constitutes the ExpressionEditor
      */
     private static class MouseEventHandler implements EventHandler<MouseEvent> {
@@ -33,9 +94,13 @@ public class ExpressionEditor extends Application {
 
         public void handle(MouseEvent event) {
             //System.out.println(rootExp.convertToString(0));
+            /**
+             * Current Issues:
+             * No repaints. or whatever javafx does
+             * No focus change on mouse click due to bounding box issues. 
+             */
             expressionPane.requestLayout();
             Point2D mouseCoords = new Point2D(event.getX(), event.getY());
-
             if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
                 System.out.println("Mouse Event at " + mouseCoords.toString());
                 for (Expression e : expression.children) {
@@ -56,78 +121,5 @@ public class ExpressionEditor extends Application {
             }
             System.out.println(expression.getNode().getBoundsInParent());
         }
-    }
-
-    /**
-     * Size of the GUI
-     */
-    private static final int WINDOW_WIDTH = 1080, WINDOW_HEIGHT = 250;
-
-    /**
-     * Initial expression shown in the textbox
-     */
-    private static final String EXAMPLE_EXPRESSION = "2*x+3*y+4*z+(7+6*z)";
-    //private static final String EXAMPLE_EXPRESSION = "a+b+c";
-
-    /**
-     * Parser used for parsing expressions.
-     */
-    private final ExpressionParser expressionParser = new SimpleExpressionParser();
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Expression Editor");
-
-        // Add the textbox and Parser button
-        final Pane queryPane = new HBox();
-        final TextField textField = new TextField(EXAMPLE_EXPRESSION);
-        final Button button = new Button("Parse");
-        queryPane.getChildren().add(textField);
-
-
-        // Add the callback to handle when the Parse button is pressed
-        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent e) {
-                // Try to parse the expression
-                try {
-                    // Success! Add the expression's Node to the expressionPane
-                    final Expression expression = expressionParser.parse(textField.getText(), true);
-                    expressionPane.getChildren().clear();
-                    Node node = expression.getNode();
-                    expressionPane.setStyle("-fx-font:36 \"Arial\";");
-
-                    node.setLayoutX(WINDOW_WIDTH / 4);
-                    node.setLayoutY(WINDOW_HEIGHT / 4);
-                    node.applyCss();
-                    node.toFront();
-                    expressionPane.getChildren().add(node);
-
-
-                    // If the parsed expression is a CompoundExpression, then register some callbacks
-                    if (expression instanceof CompoundExpression) {
-                        ((Pane) expression.getNode()).setBorder(Expression.NO_BORDER);
-                        final MouseEventHandler eventHandler = new MouseEventHandler(expressionPane, (CompoundExpression) expression);
-                        expressionPane.setOnMousePressed(eventHandler);
-                        expressionPane.setOnMouseDragged(eventHandler);
-                        expressionPane.setOnMouseReleased(eventHandler);
-                    }
-                } catch (ExpressionParseException epe) {
-                    // If we can't parse the expression, then mark it in red
-                    textField.setStyle("-fx-text-fill: red");
-                }
-            }
-        });
-        queryPane.getChildren().add(button);
-
-        // Reset the color to black whenever the user presses a key
-        textField.setOnKeyPressed(e -> textField.setStyle("-fx-text-fill: black"));
-
-        final BorderPane root = new BorderPane();
-        root.setTop(queryPane);
-        root.setCenter(expressionPane);
-        expressionPane.applyCss();
-        expressionPane.layout();
-        primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
-        primaryStage.show();
     }
 }
